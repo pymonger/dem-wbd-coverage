fef30d0554edf490f1be9291b6fca9bf8afa3cb8
Gerald Manipon <geraldjohn.m.manipon@jpl.nasa.gov>
2019-01-17 12:58:06 -0800

    download catalog and view coverage from catalog

--- README.md
+++ README.md
@@ -6,11 +6,44 @@ Utility to display DEM or WBD tile coverage
 - basemap
 - pandas
 - geos
+- awscli
 - e.g.
   ```
   conda install basemap pandas geos
+  conda install -c conda-forge awscli
   ```
-- export PROJ_LIB:
+- export *PROJ_LIB*:
   ```
   export PROJ_LIB=${HOME}/anaconda3/pkgs/proj4-5.2.0-h1de35cc_1001/share/proj
   ```
+
+## Download catalog of tiles
+1. Set bucket:
+   ```
+   export BUCKET=s3://grfn-v2-ops-product-bucket
+   ```
+1. Get catalog of tiles for SRTM1_v3:
+   ```
+   aws s3 ls ${BUCKET}/datasets/dem/SRTM1_v3/ | awk '{print $4}' | grep -v '^\.' > SRTM1_v3.txt
+   ```
+1. Get catalog of tiles for NED1:
+   ```
+   aws s3 ls ${BUCKET}/datasets/dem/ned1/ | awk '{print $4}' | grep -v '^\.' > NED1.txt
+   ```
+1. Get catalog of tiles for NED13:
+   ```
+   aws s3 ls ${BUCKET}/datasets/dem/ned13/ | awk '{print $4}' | grep -v '^\.' > NED13.txt
+   ```
+1. Get catalog of tiles for SRTM WBD:
+   ```
+   aws s3 ls ${BUCKET}/datasets/dem/usgs_mirror/SRTMSWBD.003/2000.02.11/ | awk '{print $4}' | grep -v '^\.' | grep -v .zip.xml > SWBD.txt
+   ```
+
+## View coverage
+```
+./plot_coverage.py <catalog file>
+```
+E.g.
+```
+./plot_coverage.py SRTM1_v3.txt
+```
--- plot_coverage.py
+++ plot_coverage.py
@@ -1,31 +1,47 @@
 #!/usr/bin/env python
 import sys
+import re
 from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
 import pandas as pd
 
 
-def plot_coverage(csv_file):
+TILE_RE = re.compile(
+    r'^(?P<lat_orient>N|S)(?P<lat>\d+)(?P<lon_orient>E|W)(?P<lon>\d+)')
+
+
+def get_tiles_from_catalog(catalog_file):
+    """Parse catalog of tiles and return pandas data frame of
+       tile locations."""
+
+    lats = []
+    lons = []
+    with open(catalog_file) as f:
+        for line in f:
+            match = TILE_RE.search(line)
+            if not match:
+                raise(RuntimeError("Failed to extract tile info: {}".format(line)))
+            m = match.groupdict()
+            lats.append(-int(m['lat']) if m['lat_orient']
+                        == 'S' else int(m['lat']))
+            lons.append(-int(m['lon']) if m['lon_orient']
+                        == 'W' else int(m['lon']))
+    return pd.DataFrame({'lat': lats, 'lon': lons})
+
+
+def plot_coverage(tiles):
     """Plot coverage."""
 
-    map = Basemap(projection="cyl", llcrnrlat=-90, urcrnrlat=90,
-                  llcrnrlon=-180, urcrnrlon=180, resolution='c')
-    swbd = pd.read_csv(csv_file, header=None, names=['lat', 'lon'])
-    lons = swbd.lon
-    lats = swbd.lat
-    m = Basemap(projection="cyl", lon_0=0, lat_0=0, resolution='c')
-    m.drawcoastlines()
-    x, y = map(lons, lats)
-    x, y = m(lons, lats)
-    m.scatter(x, y, marker='D')
-    plt.show()
-    plt.show()
+    lons = tiles.lon
+    lats = tiles.lat
     m = Basemap(projection="cyl", lon_0=0, lat_0=0, resolution='c')
     m.drawcoastlines()
     x, y = m(lons, lats)
-    m.scatter(x, y, marker='D')
+    m.scatter(x, y, marker='o', s=1)
     plt.show()
+    # plt.savefig('test.tif')
 
 
 if __name__ == "__main__":
-    plot_coverage(sys.argv[1])
+    tiles = get_tiles_from_catalog(sys.argv[1])
+    plot_coverage(tiles)
